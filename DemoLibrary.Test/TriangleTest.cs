96524a8fe4b17774754e0f6ccb60bc271bfcbf88
ioannis <ioaznnis@outlook.com>
2019-09-25 15:36:27 +0300

    Новый вариант треугольника

---
+++ DemoLibrary.Test/TriangleTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DemoLibrary.Test
+{
+	[TestFixture]
+    public class TriangleTest
+    {
+        [TestCase(9, 9, 9, 35.074d)]
+        [TestCase(2, 3, 4, 2.9047d)]
+        [TestCase(3, 4, 5, 6)]
+        public void TriAngleFactory_CalcArea(double a, double b, double c, double area)
+        {
+            Assert.LessOrEqual(Math.Abs(new Triangle(a, b, c).CalcArea() - area), 0.0001d);
+        }
+    }
+}
---
+++ DemoLibrary/Triangle.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using DemoLibrary.Exception;
+
+namespace DemoLibrary
+{
+    /// <summary>
+    /// Альтернативный вариант класса "треугольник", так как нам надо знать только площадь
+    /// </summary>
+    public class Triangle : IFigure
+    {
+        /// <summary>
+        /// Массив сторон, упорядоченный по убыванию, т.е. первый элемент - возможно гипотенуза
+        /// </summary>
+        private readonly double[] _parts;
+
+        public Triangle(double a, double b, double c)
+        {
+            var parts = new[] {a, b, c}.OrderByDescending(d => d).ToArray();
+
+            if (parts.Any(x => x < 0))
+            {
+                throw new NegativeLengthException();
+            }
+
+            if (parts.Any(x => Math.Abs(x) < Constants.Tolerance))
+            {
+                throw new ZeroLengthException();
+            }
+
+            _parts = parts;
+        }
+
+        /// <summary>
+        /// Подсчет суммы квадратов катетов минус квадрат гипотенузы
+        /// </summary>
+        /// <returns></returns>
+        private double PythagorasTheorem()
+        {
+            var accumulate = 0d;
+
+            var first = true;
+            foreach (var part in _parts)
+            {
+                if (first)
+                {
+                    accumulate -= part * part;
+                    first = false;
+                }
+                else
+                {
+                    accumulate += part * part;
+                }
+            }
+
+            return accumulate;
+        }
+
+        public double CalcArea()
+        {
+            if (Math.Abs(PythagorasTheorem()) < Constants.Tolerance)
+            {
+                //Если теорема пифагора выполняется, то _parts[0] - гипотенуза, а остальные - катеты
+                return _parts[1] * _parts[2] / 2;
+            }
+
+            var semiPerimeter = _parts.Sum() / 2;
+            return Math.Sqrt(semiPerimeter * _parts.Select(x => semiPerimeter - x).Aggregate((x, y) => x * y));
+        }
+    }
+}
